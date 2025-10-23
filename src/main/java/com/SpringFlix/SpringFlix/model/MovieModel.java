2dffc928a5eafd1f528745220a4332d6bdd1bfa6
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-23 13:59:51 -0400

    feat: add movie model

---
+++ src/main/java/com/SpringFlix/SpringFlix/model/MovieModel.java
@@ -0,0 +1,25 @@
+package com.SpringFlix.SpringFlix.model;
+
+import com.SpringFlix.SpringFlix.enums.MovieCategory;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name="Movie_Item")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieModel {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String title;
+    private String synopsys;
+    private String image;
+    private LocalDate releaseDate;
+    private MovieCategory movieCategory;
+}
