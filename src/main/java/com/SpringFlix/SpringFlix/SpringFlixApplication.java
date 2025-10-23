662e18b15ffcac8c4b9bcaff3f70666358169762
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-22 20:49:02 -0400

    chore(init): set up initial Spring Boot project structure

---
+++ src/main/java/com/SpringFlix/SpringFlix/SpringFlixApplication.java
@@ -0,0 +1,13 @@
+package com.SpringFlix.SpringFlix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFlixApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringFlix/SpringFlix/SpringFlixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringFlix.SpringFlix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFlixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
