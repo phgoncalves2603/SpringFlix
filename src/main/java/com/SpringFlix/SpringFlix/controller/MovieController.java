a2ed4439207603a1f55c25c54aaa7546a86e135d
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-23 13:59:17 -0400

    feat: create controller

---
+++ src/main/java/com/SpringFlix/SpringFlix/controller/MovieController.java
@@ -0,0 +1,11 @@
+package com.SpringFlix.SpringFlix.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/movie/")
+public class MovieController {
+
+}
