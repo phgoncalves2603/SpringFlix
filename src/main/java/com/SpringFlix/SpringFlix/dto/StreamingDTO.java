8f87f37c294ea528b1529030884ad1a97b710c34
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-25 23:36:06 -0400

    feat: add streamingDTO

---
+++ src/main/java/com/SpringFlix/SpringFlix/dto/StreamingDTO.java
@@ -0,0 +1,13 @@
+package com.SpringFlix.SpringFlix.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class StreamingDTO {
+    private Long id;
+    private String name;
+}
