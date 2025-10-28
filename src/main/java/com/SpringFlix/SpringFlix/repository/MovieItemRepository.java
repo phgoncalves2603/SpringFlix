bb485774cc45148f7f6773dddcbd2a7a93f6dd34
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-27 21:41:14 -0400

    feat: add movie Repository

--- src/main/java/com/SpringFlix/SpringFlix/repository/MovieItemRepository.java
+++ src/main/java/com/SpringFlix/SpringFlix/repository/MovieItemRepository.java
@@ -2,6 +2,8 @@ package com.SpringFlix.SpringFlix.repository;
 
 import com.SpringFlix.SpringFlix.model.MovieModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MovieItemRepository extends JpaRepository<MovieModel,Long> {
 }
