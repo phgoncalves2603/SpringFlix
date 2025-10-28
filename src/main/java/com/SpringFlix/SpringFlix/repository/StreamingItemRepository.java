75cd6690a3bdd9e1646660b3d86e4c8e7da3dd2d
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-27 21:40:23 -0400

    feat: add Streaming Repository

--- src/main/java/com/SpringFlix/SpringFlix/repository/StreamingItemRepository.java
+++ src/main/java/com/SpringFlix/SpringFlix/repository/StreamingItemRepository.java
@@ -2,6 +2,8 @@ package com.SpringFlix.SpringFlix.repository;
 
 import com.SpringFlix.SpringFlix.model.StreamingModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface StreamingItemRepository extends JpaRepository<StreamingModel,Long> {
 }
