f3c259c3d2d4763f60d3cee321e47ee94dcc4556
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-27 21:44:05 -0400

    feat: add Streaming mapper

--- src/main/java/com/SpringFlix/SpringFlix/mapper/StreamingMapper.java
+++ src/main/java/com/SpringFlix/SpringFlix/mapper/StreamingMapper.java
@@ -1,9 +1,12 @@
-package com.SpringFlix.SpringFlix.dto;
+package com.SpringFlix.SpringFlix.mapper;
 
+import com.SpringFlix.SpringFlix.dto.StreamingDTO;
 import com.SpringFlix.SpringFlix.model.StreamingModel;
+import lombok.experimental.UtilityClass;
 
-public class StreamingMapper {
-    public StreamingDTO map(StreamingModel streamingModel){
+@UtilityClass
+public class  StreamingMapper {
+    public static StreamingDTO map(StreamingModel streamingModel){
         StreamingDTO streamingDTO = new StreamingDTO();
         streamingDTO.setId(streamingModel.getId());
         streamingDTO.setName(streamingModel.getName());
@@ -11,7 +14,7 @@ public class StreamingMapper {
 
         return streamingDTO;
     }
-    public StreamingModel map(StreamingDTO streamingDTO){
+    public static StreamingModel map(StreamingDTO streamingDTO){
         StreamingModel streamingModel= new StreamingModel();
         streamingModel.setId(streamingDTO.getId());
         streamingModel.setName(streamingDTO.getName());
