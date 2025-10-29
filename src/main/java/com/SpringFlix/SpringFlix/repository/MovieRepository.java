43cd78cc5bb53c213e53c5b72a0e9253deb57361
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-29 13:08:54 -0400

    feat: add model service

--- src/main/java/com/SpringFlix/SpringFlix/repository/MovieRepository.java
+++ src/main/java/com/SpringFlix/SpringFlix/repository/MovieRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface MovieItemRepository extends JpaRepository<MovieModel,Long> {
+public interface MovieRepository extends JpaRepository<MovieModel,Long> {
 }
