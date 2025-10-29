0a2f10b3f8e23fa6ad3afb76586fe4abf3e9a891
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-10-29 12:49:28 -0400

    Refactor(rename): rename Streaming repository

--- src/main/java/com/SpringFlix/SpringFlix/repository/StreamingRepository.java
+++ src/main/java/com/SpringFlix/SpringFlix/repository/StreamingRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface StreamingItemRepository extends JpaRepository<StreamingModel,Long> {
+public interface StreamingRepository extends JpaRepository<StreamingModel,Long> {
 }
