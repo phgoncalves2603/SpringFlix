5d16262b4042a9192d96d069507f05e17d3bf1c3
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-02 21:38:19 -0500

    feat: Extend UserDetails

--- src/main/java/com/SpringFlix/SpringFlix/model/UsersModel.java
+++ src/main/java/com/SpringFlix/SpringFlix/model/UsersModel.java
@@ -1,9 +1,17 @@
 package com.SpringFlix.SpringFlix.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
 
 @Entity(name = "users")
-public class UsersModel {
+@Getter
+@Setter
+public class UsersModel implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -13,4 +21,31 @@ public class UsersModel {
     private String email;
     @Column(nullable = false)
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;    }
 }
