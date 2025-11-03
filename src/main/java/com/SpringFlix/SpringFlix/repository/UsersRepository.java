fbf74485928f875433661cbdbe93543cddfe9fce
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-02 21:39:01 -0500

    feat: add findbyEmail function

--- src/main/java/com/SpringFlix/SpringFlix/repository/UsersRepository.java
+++ src/main/java/com/SpringFlix/SpringFlix/repository/UsersRepository.java
@@ -2,6 +2,11 @@ package com.SpringFlix.SpringFlix.repository;
 
 import com.SpringFlix.SpringFlix.model.UsersModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Optional;
 
 public interface UsersRepository extends JpaRepository<UsersModel,Long> {
+
+    Optional<UserDetails> findByEmail(String email);
 }
