2524181d0116b6bc25ff2fc4eb28f7b5b727bb2a
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-02 21:40:11 -0500

    feat: add save, with password encryption on the database

--- src/main/java/com/SpringFlix/SpringFlix/service/UsersService.java
+++ src/main/java/com/SpringFlix/SpringFlix/service/UsersService.java
@@ -1,14 +1,22 @@
 package com.SpringFlix.SpringFlix.service;
 
+import com.SpringFlix.SpringFlix.model.UsersModel;
 import com.SpringFlix.SpringFlix.repository.UsersRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class UsersService {
-    final private UsersRepository usersRepository;
+    private final UsersRepository usersRepository;
+    private final PasswordEncoder passwordEncoder;
 
+    public UsersModel save(UsersModel usersModel){
+        String password = usersModel.getPassword();
+        usersModel.setPassword(passwordEncoder.encode(password)); //encoding password before save into db
+        return  usersRepository.save(usersModel);
+    }
 
 
 }
