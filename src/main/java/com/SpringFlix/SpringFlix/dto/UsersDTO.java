1b571151e235ab20e7f48ea9152e9144f2d7a481
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-02 21:43:44 -0500

    feat: add users dto

---
+++ src/main/java/com/SpringFlix/SpringFlix/dto/UsersDTO.java
@@ -0,0 +1,14 @@
+package com.SpringFlix.SpringFlix.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UsersDTO {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+
+}
