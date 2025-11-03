192b28a5316bc27b53a706a07e0b9afb818a656a
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-02 21:42:43 -0500

    feat: add tokenDTO stores the section token

---
+++ src/main/java/com/SpringFlix/SpringFlix/dto/TokenDTO.java
@@ -0,0 +1,10 @@
+package com.SpringFlix.SpringFlix.dto;
+
+import lombok.RequiredArgsConstructor;
+
+public class TokenDTO {
+    private String token;
+    public TokenDTO(String token){
+        this.token = token;
+    }
+}
