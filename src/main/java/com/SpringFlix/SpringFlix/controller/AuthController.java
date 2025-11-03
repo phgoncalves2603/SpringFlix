1d9fa6e734d2e35d291399ef21c219d7b7eb67db
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-02 21:40:51 -0500

    feat: add register and user login endpoint

---
+++ src/main/java/com/SpringFlix/SpringFlix/controller/AuthController.java
@@ -0,0 +1,52 @@
+package com.SpringFlix.SpringFlix.controller;
+
+
+import com.SpringFlix.SpringFlix.config.TokenService;
+import com.SpringFlix.SpringFlix.dto.LoginDTO;
+import com.SpringFlix.SpringFlix.dto.TokenDTO;
+import com.SpringFlix.SpringFlix.dto.UsersDTO;
+import com.SpringFlix.SpringFlix.mapper.UsersMapper;
+import com.SpringFlix.SpringFlix.model.UsersModel;
+import com.SpringFlix.SpringFlix.service.UsersService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final UsersService usersService;
+    private final AuthenticationManager authenticationManager;
+    private final TokenService tokenService;
+    
+
+    @PostMapping("/register")
+    public ResponseEntity<UsersDTO> save(@RequestBody UsersDTO usersDTO){
+        usersService.save(UsersMapper.map(usersDTO));
+        return ResponseEntity.status(HttpStatus.CREATED).body(usersDTO);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<TokenDTO> login(@RequestBody LoginDTO loginDTO){
+        UsernamePasswordAuthenticationToken userAndPass = new UsernamePasswordAuthenticationToken(loginDTO.getEmail(),loginDTO.getPassword());
+        Authentication authenticate = authenticationManager.authenticate(userAndPass);
+
+        UsersModel user = (UsersModel) authenticate.getPrincipal();
+
+        String token = tokenService.GenerateToken(user);
+
+        return ResponseEntity.ok(new TokenDTO(token));
+
+
+    }
+
+
+}
