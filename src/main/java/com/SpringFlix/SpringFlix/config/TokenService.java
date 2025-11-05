33fb6993463b2111fe37b8be04aa2ec6b2ef0871
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-04 21:32:55 -0500

    feat: add verify token method

--- src/main/java/com/SpringFlix/SpringFlix/config/TokenService.java
+++ src/main/java/com/SpringFlix/SpringFlix/config/TokenService.java
@@ -1,13 +1,17 @@
 package com.SpringFlix.SpringFlix.config;
 
 
+import com.SpringFlix.SpringFlix.dto.JWTUserDataDTO;
 import com.SpringFlix.SpringFlix.model.UsersModel;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.time.Instant;
+import java.util.Base64;
+import java.util.Optional;
 
 @Component
 public class TokenService {
@@ -24,6 +28,20 @@ public class TokenService {
                 .withIssuedAt(Instant.now())
                 .withIssuer("API SpringFlix")
                 .sign(algorithm);
+    }
+
+    public Optional<JWTUserDataDTO> verifyToken(String token){
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        DecodedJWT jwt = JWT.require(algorithm)
+                .build()
+                .verify(token);
+        JWTUserDataDTO jwtUserDataDTO = new JWTUserDataDTO();
+        jwtUserDataDTO.setId(jwt.getClaim("userId").asLong());
+        jwtUserDataDTO.setName(jwt.getClaim("name").asString());
+        jwtUserDataDTO.setEmail(jwt.getSubject());
+
+        return Optional.of(jwtUserDataDTO);
     }
 
 }
