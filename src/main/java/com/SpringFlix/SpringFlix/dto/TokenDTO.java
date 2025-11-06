8905ebf89ae8e79a6c6ff1cb45e479aae67e177e
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-05 20:48:38 -0500

    fix: add getters and setter with lombok

--- src/main/java/com/SpringFlix/SpringFlix/dto/TokenDTO.java
+++ src/main/java/com/SpringFlix/SpringFlix/dto/TokenDTO.java
@@ -1,7 +1,11 @@
 package com.SpringFlix.SpringFlix.dto;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
 public class TokenDTO {
     private String token;
     public TokenDTO(String token){
