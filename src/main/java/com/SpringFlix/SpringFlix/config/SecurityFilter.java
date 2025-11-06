1b6ea0dd6d4c9cc5306b956dc5505f00e3f2a654
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-05 20:49:48 -0500

    fix(auth-filter): skip authentication for /auth/login and /auth/register endpoints

--- src/main/java/com/SpringFlix/SpringFlix/config/SecurityFilter.java
+++ src/main/java/com/SpringFlix/SpringFlix/config/SecurityFilter.java
@@ -24,6 +24,14 @@ public class SecurityFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String path = request.getServletPath();
+
+        if (path.equals("/auth/login") || path.equals("/auth/register")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+
         String authorizationHeader = request.getHeader("Authorization");
 
         if (Strings.isNotEmpty(authorizationHeader) && authorizationHeader.startsWith("Bearer ")) {
