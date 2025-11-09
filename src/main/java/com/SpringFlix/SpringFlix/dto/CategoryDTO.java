8a24c7a669493bd556e6473a31ac761e0f0b62ee
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-08 23:07:32 -0500

    refactor: add NotEmpty notation on name

--- src/main/java/com/SpringFlix/SpringFlix/dto/CategoryDTO.java
+++ src/main/java/com/SpringFlix/SpringFlix/dto/CategoryDTO.java
@@ -1,5 +1,6 @@
 package com.SpringFlix.SpringFlix.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -7,5 +8,6 @@ import lombok.Setter;
 @Getter
 public class CategoryDTO {
     private Long id;
+    @NotEmpty(message = "Category is needed")
     private String name;
 }
