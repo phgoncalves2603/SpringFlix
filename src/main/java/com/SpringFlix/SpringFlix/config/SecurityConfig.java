0696595e04f38249e35981f3d04d89b9f1df8add
phgoncalves2603 <phgoncalves2603@gmail.com>
2025-11-08 23:09:59 -0500

    refactor: add dispatcher

--- src/main/java/com/SpringFlix/SpringFlix/config/SecurityConfig.java
+++ src/main/java/com/SpringFlix/SpringFlix/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.SpringFlix.SpringFlix.config;
 
+import jakarta.servlet.DispatcherType;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -29,6 +30,7 @@ public class SecurityConfig {
                     .csrf(csrf -> csrf.disable())
                     .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                     .authorizeHttpRequests(auth -> auth
+                            .dispatcherTypeMatchers(DispatcherType.ERROR).permitAll()
                             .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                             .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
                             .anyRequest().authenticated()
